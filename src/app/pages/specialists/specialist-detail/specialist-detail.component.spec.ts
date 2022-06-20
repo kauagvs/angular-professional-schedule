4b3d37dae68a4e05fb0b265a8b2cf0d227105a76
DESKTOP-SEMENOV\kauas <kaua.semenov@gmail.com>
2022-06-20 19:41:58 -0300

    test: tests in specialist detail component

--- src/app/pages/specialists/specialist-detail/specialist-detail.component.spec.ts
+++ src/app/pages/specialists/specialist-detail/specialist-detail.component.spec.ts
@@ -1,16 +1,36 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SpecialistsService } from 'src/app/core/services/specialists/specialists.service';
 
 import { SpecialistDetailComponent } from './specialist-detail.component';
 
 describe('SpecialistDetailComponent', () => {
   let component: SpecialistDetailComponent;
   let fixture: ComponentFixture<SpecialistDetailComponent>;
+  const fakeActivatedRoute = {
+    snapshot: {
+      params: {
+        get(): string {
+          return '/';
+        },
+      },
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SpecialistDetailComponent ]
-    })
-    .compileComponents();
+      declarations: [SpecialistDetailComponent],
+      providers: [
+        SpecialistsService,
+        {
+          provide: ActivatedRoute,
+          useValue: fakeActivatedRoute,
+        },
+      ],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SpecialistDetailComponent);
     component = fixture.componentInstance;
